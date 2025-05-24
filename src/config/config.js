7103c73189cb38ab3525d6df3b46fed78e6842de
Joan Baron <joansebaron@gmail.com>
2025-05-23 19:35:32 -0500

    Subida inicial del proyecto Backend Node + Mongo

--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 const conectarDB = async () => {
   try {
     const connectionDB = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      /*useNewUrlParser: true,
+      useUnifiedTopology: true,*/
     });
 
     const url = `CONECTADO A MONGODB EN SERVER (HOST): ${connectionDB.connection.host} 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/config.js";
+import router from "./routes/categorias.routes.js";
+import Categoria from "./models/Categorias.js";
 const app = express();
 
 dotenv.config();
+
+app.use("/categoria",router);
+
 
 const PORT = process.env.PORT;
 
